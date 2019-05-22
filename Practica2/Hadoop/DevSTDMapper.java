6ec960301a6b8045a312b3499d9ded225fcd5d17
Adrián de la Torre Rodríguez <atr96@correo.ugr.es>
2019-05-22 12:17:01 +0200

    Arreglo doublewritable

--- Practica2/Hadoop/DevSTDMapper.java
+++ Practica2/Hadoop/DevSTDMapper.java
@@ -12,9 +12,10 @@ public class DevSTDMapper extends Mapper<Object, Text, Text, DoubleWritable>{
    public void map(Object key, Text value, Context context) throws IOException {
 
      String[] fields = value.toString().split(",");
+
      if(fields[0] != "f1"){
        for(int i = 0; i<fields.length; i++) {
-         context.write(new Text(Integer.toString(i)), Double.parseDouble(fields[i]));
+         context.write(new Text(Integer.toString(i)), new DoubleWritable(Double.parseDouble(fields[i])));
        }
      }
    }
